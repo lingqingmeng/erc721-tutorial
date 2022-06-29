0b9e7b1b0b801f2fbf8c86e2124aaf908007c474
Ling Qing Meng <lingqingmeng@users.noreply.github.com>
2022-06-28 21:55:39 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,6 @@ async function main() {
    const Pairing = await ethers.getContractFactory("Pairing");
 
    // Start deployment, returning a promise that resolves to a contract object
-   console.log("Pairing",Pairing);
-   console.log("Pairing.deploy: ",Pairing.deploy)
    const pairing = await Pairing.deploy();
    console.log("Contract deployed to address:", pairing.address);
 }
